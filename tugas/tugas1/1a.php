9c48c79dd28ed965c5edbddc3184c0dda0fa4da8
chandafa <chankirana722@gmail.com>
2024-02-16 21:53:27 +0700

    selesai

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,7 @@
+<?php
+$topi = "Topi";
+$bundar = "Bundar";
+
+echo "<h2><em>" . " ”" . $topi . " Saya Bundar, " . $bundar . " Topi saya.”" . "</em></h2>";
+
+?>
---
+++ tugas/tugas1/1b.php
@@ -0,0 +1,12 @@
+<?php
+$npm_candra = 37; // Ganti dengan dua digit terakhir NPM Anda
+echo "Aku adalah angka <strong> $npm_candra </strong> <br>";
+$npm_candra *= 5;
+echo "Jika aku dikali 5, maka aku sekarang menjadi <strong> $npm_candra </strong> <br>";
+$npm_candra /= 2;
+echo "Jika aku dibagi 2, maka aku sekarang menjadi <strong> $npm_candra </strong> <br>";
+$npm_candra += 75;
+echo "Jika aku ditambah 75, maka aku sekarang menjadi <strong> $npm_candra </strong> <br>";
+$npm_candra -= 20;
+echo "Jika aku dikurang 20, maka aku sekarang menjadi <strong> $npm_candra </strong> <br>";
+?>
